6e3d957d4d0d5fc70f6c7007bb21e020bb341dc0
BillStephens2022 <two4onebill@yahoo.com>
2023-09-12 16:12:01 -0400

    updated html and css styling, added app screenshot

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-
+  @Input() cardNumber: string | null;
+  @Input() name: string | null;
+  @Input() expiration: string | null;
+  constructor(){
+    this.cardNumber='';
+    this.name=''
+    this.expiration='';
+  }
 }
